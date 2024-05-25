ac767a155de2e065838e1adfc95f96d9dccc9b40
Magdiel Gomes Ferreira <magdielgomesferreira@gmail.com>
2024-05-25 11:44:31 -0300

    AtualizaçãoPastas

---
+++ js/produtoacao.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', (event) =>{
+    
+    let adicionar = document.getElementById('adicionarproduto');
+    
+    let remover = document.getElementById('removerproduto');
+
+    let categoria
+
+});
